d13642e62578c7c654721e0f7116bdce13856cf1
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-24 15:32:51 +0000

    fix other merge conflict

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from '../App';
-=======
-import App from './../App';
->>>>>>> 6041f46f8753bf1cd7d81ff75e0f1c54ee7727b2
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
